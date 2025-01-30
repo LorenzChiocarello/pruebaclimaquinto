4cde9bbf4c766e7fc05390094267b3399e4dc4f3
Lorenzo Chiocarello <121979179+LorenzChiocarello@users.noreply.github.com>
2025-01-30 11:04:41 -0300

    Backend y frontend

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from utils.db import app
 from routes.valores_routes import valores_bp
+from flask_cors import CORS
 
 app.register_blueprint(valores_bp)
+
+CORS(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
